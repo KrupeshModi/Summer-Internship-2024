13bc35b3c605da8af8be2ce3308c4b5de40d2194
DhruvParmar1402 <parmardhruv924.com>
2024-06-06 09:06:04 +0530

    file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className='text-red-800'>
-      hello 
-    </div>
-  )
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
 }
+
+export default App;
